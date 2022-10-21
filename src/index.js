7199b51f6cda5c8843f0457082ad2af705bba42b
Spyrosgg <spyrosgg@protonmail.com>
2022-10-22 00:44:03 +0300

    slit code & some cleaning

--- src/index.js
+++ src/index.js
@@ -1,39 +1,30 @@
 import "./style.css";
+import dataTestIn from './pages/testData';
+
+//testing data manually entered
+const objArr= dataTestIn();
 
 const content = document.querySelector("#content");
 const inbox = [];
 
-const objArr = [
-  [
-    "1my first title",
-    "this is thesting ground",
-    "2022-10-22",
-    1,
-    "Some notes go here. More notes",
-  ],
-  [
-    "2my second title",
-    "whatnot",
-    "2022-05-12",
-    2,
-    "Some notes go here. More notes",
-  ],
-  [
-    "3my third title",
-    "whatever you want",
-    "2022-10-22",
-    3,
-    "Some notes go here. More notes",
-  ],
-];
 
+
+
+
+//injects the objs into main #content---------
 objArr.forEach((el) => {
-  let objTodo = todoFactory(el);
-  console.log(objTodo);
-  let todo = componentDiv(["div", "todo", objTodo]);
+  let todoObj = todoFactory(el);
+  console.log(todoObj);
+
+  let todoDiv = createElement("div", "todo");
+  let todo = renderObj(todoDiv,todoObj);
   content.appendChild(todo);
 });
 
+
+//---------------------------------------------
+
+//todo object factory
 function todoFactory([title, description, dueDate, priority, notes]) {
   const date = new Date();
   const newdate = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}, ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
@@ -48,19 +39,21 @@ function todoFactory([title, description, dueDate, priority, notes]) {
   };
 }
 
-function componentDiv([tagType, classDef, input]) {
+function renderObj (element,input){
+    
+    if (typeof input === "object") {
+      let string = "";
+      for (var prop in input) {
+        string += `<i>${prop}</i> : ${input[prop]}</br>`;
+      }
+      element.innerHTML = string;
+      return element;
+    }
+    console.log('Error: not an object');
+};    
+
+function createElement(tagType, classDef) {
   const element = document.createElement(tagType);
   element.classList.add(classDef);
-
-  if (typeof input === "object") {
-    let string = "";
-    for (var prop in input) {
-      string += `<i>${prop}</i> : ${input[prop]}</br>`;
-    }
-    element.innerHTML = string;
-    return element;
-  }
-  element.textContent = input;
-
   return element;
-}
+};
---
+++ src/pages/testData.js
@@ -0,0 +1,27 @@
+export default function testData() {
+  const obj = [
+    [
+      "1my first title",
+      "this is thesting ground",
+      "2022-10-22",
+      1,
+      "Some notes go here. More notes",
+    ],
+    [
+      "2my second title",
+      "whatnot",
+      "2022-05-12",
+      2,
+      "Some notes go here. More notes",
+    ],
+    [
+      "3my third title",
+      "whatever you want",
+      "2022-10-22",
+      3,
+      "Some notes go here. More notes",
+    ],
+  ];
+  return obj;
+
+}
